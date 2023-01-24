b2b9d29edbeaa62b55b66cee998bb4674fcefc49
bek <geebrox@gmail.com>
2023-01-24 19:23:36 +0500

    feat: add handlebars to express as view engine, create base layout and home view

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,24 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
+import path from 'path';
 
-const server = express();
+const viewsDir = path.resolve(__dirname, 'views');
+const server = express()
+  .engine(
+    '.hbs',
+    engine({
+      extname: '.hbs',
+      layoutsDir: path.resolve(viewsDir, 'layouts'),
+      partialsDir: path.resolve(viewsDir, 'partials'),
+      defaultLayout: 'default',
+    })
+  )
+  .set('view engine', '.hbs')
+  .set('views', viewsDir);
+
+server.get('/', (_, res) => {
+  res.render('home', { title: 'Home' });
+});
 
 server.listen(3000, () => {
   console.log('🚀 Server has been started: http://localhost:3000');
