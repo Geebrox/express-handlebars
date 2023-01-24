26d9687892acbc55e7d75a7840eafb0fb2437392
bek <geebrox@gmail.com>
2023-01-24 23:18:37 +0500

    feat: add view routes and styles for navigation

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,24 @@ import express from 'express';
 import { engine } from 'express-handlebars';
 import path from 'path';
 
+const VIEW_ROUTES = [
+  {
+    path: '/',
+    title: 'Home',
+    view: 'home',
+  },
+  {
+    path: '/message-board',
+    title: 'Message board',
+    view: 'message-board',
+  },
+  {
+    path: '/average-number',
+    title: 'Average number',
+    view: 'average-number',
+  },
+];
+
 const viewsDir = path.resolve(__dirname, 'views');
 const publicDir = path.resolve(__dirname, 'public');
 const server = express()
@@ -12,14 +30,34 @@ const server = express()
       layoutsDir: path.resolve(viewsDir, 'layouts'),
       partialsDir: path.resolve(viewsDir, 'partials'),
       defaultLayout: 'default',
+      helpers: {
+        isActiveRoute: (
+          a: (typeof VIEW_ROUTES)[0],
+          b: (typeof VIEW_ROUTES)[0]
+        ) => {
+          return a.path == b.path;
+        },
+      },
     })
   )
   .set('view engine', '.hbs')
   .set('views', viewsDir)
   .use(express.static(publicDir));
 
-server.get('/', (_, res) => {
-  res.render('home', { title: 'Home' });
+server.use((req, res, next) => {
+  const foundViewRoute = VIEW_ROUTES.find((viewRoute) => {
+    return viewRoute.path === req.path;
+  });
+
+  if (foundViewRoute) {
+    res.render(foundViewRoute.view, {
+      routes: VIEW_ROUTES,
+      activeRoute: foundViewRoute,
+    });
+    return;
+  }
+
+  next();
 });
 
 server.listen(3000, () => {
